f25ab32148f47d19e0115c25827a5c5a31a16506
Rachel-Cellulose <99023474+Rachel-Cellulose@users.noreply.github.com>
2024-10-09 14:17:29 +0100

    wrap useSearchParam in Suspense to cancel error

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,6 +1,16 @@
 "use client"
-export default function error({error, reset}) {
+// export default function error({error, reset}) {
+//   return (
+//     <div>you have figured it out</div>
+//   )
+// }
+
+import React from 'react'
+
+export default function error() {
   return (
-    <div>you have figured it out</div>
+    <div>
+      ERROR
+    </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import NavbarItem from './NavbarItem'
+import React, { Suspense } from "react";
+import NavbarItem from "./NavbarItem";
 
-export default function 
-Navbar() {
+export default function Navbar() {
   return (
-    <div className='flex dark:bg-blue-700 bg-gray-200 p-4 lg:text-lg justify-center gap-6'>
-      <NavbarItem title="Trending" param="fetchtrending" />
-      <NavbarItem title="Top Rated" param="fetchTopRated" />
+    <div className="flex dark:bg-blue-700 bg-gray-200 p-4 lg:text-lg justify-center gap-6">
+      <Suspense fallback={<div>Loading...</div>}>
+        <NavbarItem title="Trending" param="fetchtrending" />
+        <NavbarItem title="Top Rated" param="fetchTopRated" />
+      </Suspense>
     </div>
   );
 }
